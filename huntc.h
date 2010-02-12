51d1b6bffa3f35d44f7505e9e32092ea22f7f4f9
Dan Hirsch <thequux@flippity.com>
2010-02-12 02:40:59 -0800

    Changed something

--- huntc.h
+++ huntc.h
@@ -2,14 +2,14 @@
 #define HUNT__HUNTC_H
 
 #include "hunt.h"
-#define MAZE_WIN_HEIGHT    20
-#define MAZE_WIN_WIDTH     64
+#define MAZE_WIN_HEIGHT    (MAZEHEIGHT + 2)
+#define MAZE_WIN_WIDTH     (MAZEWIDTH + 2)
 #define MAZE_WIN_XSTART     0
 #define MAZE_WIN_YSTART     0
 #define STATUS_WIN_HEIGHT  20
-#define STATUS_WIN_WIDTH   15
-#define STATUS_WIN_XSTART   0
-#define STATUS_WIN_YSTART  65
+#define STATUS_WIN_WIDTH   13
+#define STATUS_WIN_YSTART   0
+#define STATUS_WIN_XSTART  67
 
 WINDOW* status_win;
 WINDOW* maze_win;
