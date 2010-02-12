c56ebfe321f7d88fd53eda7a0f8566eea72ab298
Sean Moon <ssamoon@ucla.edu>
2010-02-12 02:10:01 -0800

    more include guards

--- huntc.h
+++ huntc.h
@@ -1,3 +1,6 @@
+#ifndef HUNT__HUNTC_H
+#define HUNT__HUNTC_H
+
 #define MAZE_WIN_HEIGHT    20
 #define MAZE_WIN_WIDTH     64
 #define MAZE_WIN_XSTART     0
@@ -6,3 +9,5 @@
 #define STATUS_WIN_WIDTH   15
 #define STATUS_WIN_XSTART   0
 #define STATUS_WIN_YSTART  65
+
+#endif
