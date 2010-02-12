f4f2f5fe81a8ca103c6f36394498df58e059b87d
sean <sean@usher.(none)>
2010-02-12 01:14:57 -0800

    player type, maze constants, get player name

--- hunt.h
+++ hunt.h
@@ -1,4 +1,8 @@
 #define MAZEHEIGHT  20
 #define MAZEWIDTH   64
 
+typedef struct player {
+  char* name;
+} player_t;
+
 char MAZE[MAZEHEIGHT][MAZEWIDTH];
--- huntc.c
+++ huntc.c
@@ -3,16 +3,22 @@
 
 #include "hunt.h"
 
+player_t PLAYER;
+
 void init_hunt() {
-    
   /* curses magic */
   atexit((void(*)(void))endwin);
   initscr();
-
 }
 
+void setup_player() {
+  printf("Enter your codename: ");
+  scanf("%s", PLAYER.name);
+}
 
 int main(int argc, const char* argv[]){
   
+  setup_player();
+
   return 0;
 }
