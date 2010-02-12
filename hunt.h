f8d175c5b9f70bcffd4acee12693f48fe9602e24
Sean Moon <ssamoon@ucla.edu>
2010-02-12 02:01:20 -0800

    constants for windows

--- hunt.h
+++ hunt.h
@@ -1,6 +1,17 @@
-#define MAZEHEIGHT  20
-#define MAZEWIDTH   64
-#define NAME_LEN    16
+#define MAZEHEIGHT    20
+#define MAZEWIDTH     64
+#define MAZEXSTART     0
+#define MAZEYSTART     0
+
+#define STATUSHEIGHT  20
+#define STATUSWIDTH   15
+#define STATUSXSTART   0
+#define STATUSYSTART  65
+
+#define NAME_LEN      16
+
+WINDOW* status_win;
+WINDOW* maze_win;
 
 typedef struct player {
   char name[NAME_LEN];
