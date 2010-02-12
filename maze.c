dfca82c7c749ab5a7012a7897e59d4e22e641015
Dan Hirsch <thequux@flippity.com>
2010-02-12 02:53:40 -0800

    Maze printing code even less b0rked.

--- maze.c
+++ maze.c
@@ -5,7 +5,7 @@ void redraw_maze(WINDOW* win) {
 	for ( j = -1; j < MAZEHEIGHT + 1; j++ ) {
 		mvwaddch(win,j+1,0,'#');
 		for ( i = 0; i < MAZEWIDTH; i++ ) {
-			waddch(win, (MAZE[i][j].is_wall || j < 0 || j == MAZEHEIGHT) ?'#':' ');
+			waddch(win, (j < 0 || j == MAZEHEIGHT || MAZE[i][j].is_wall) ?'#':' ');
 		}
 		waddch(win,'#');
 	}
