40613874c9857a79196559b3f6a7813a2b88a774
Sean Moon <ssamoon@ucla.edu>
2010-02-14 16:09:21 -0800

    more progess, but it still aint workin

--- huntd.rb
+++ huntd.rb
@@ -2,24 +2,56 @@
 
 require 'socket'
 
+MAZEHEIGHT = 19
+MAZEWIDTH  = 63
+HUNTPORT   = 31337
+
+class HuntClient
+  def initialize(ip,port)
+    @ip = ip
+    @port = port
+  end
+end
+
 class HuntServer
 
   def initialize(port)
     @port = port
     @clients = []
+    @maze = Array.new(MAZEWIDTH) { Array.new(MAZEHEIGHT) }
+  end
+  
+  def find_client(ip) # TODO: make O(1) operation rather than O(n)
+    @clients.each do |client|
+      return client if client.ip == ip
+    end
+    nil
+  end
+  
+  def generate_maze
+    # TODO: replace with something sensical
+    n = 0
+    (0...MAZEWIDTH).each do |x|
+      (0...MAZEHEIGHT).each do |y|
+        n += 1
+        if n % 3 == 0 then
+          @maze[x][y] = 1
+        else
+          @maze[x][y] = 0
+        end
+      end
+    end
   end
 
-  def generate_map
-  end
-
-  def send_map(clientaddr)
-    map = generate_map
-    #TODO: writeme
+  def send_maze(client)
+    maze = generate_maze
+    socket = UDPSocket.open
+    socket.connect(client.ip,client.port)
+    socket.send("oh hai!!", 0, @host, @port)
   end
   
   def getclientaddr(packet)
-    #TODO: writeme
-    "127.0.0.1"
+    packet[1][3]
   end
 
   def process(packet)
@@ -28,14 +60,17 @@ class HuntServer
     cmd = packet[0] 
     clientaddr = getclientaddr(packet)
 
+    # find the client 
+    client = find_client(clientaddr)
+
     # do something with the command
     case cmd
       when "STARTGAME"
         puts "Registering a new client @ #{clientaddr}"
-        @clients.push(clientaddr)
-      when "GETMAP"
-        puts "Sending a map to #{clientaddr}"
-        send_map(clientaddr)
+        @clients.push(HuntClient.new(clientaddr,HUNTPORT))
+      when "GETMAZE"
+        puts "Sending a maze to #{clientaddr}"
+        send_maze(client)
       else
         puts "Unknown command #{cmd}"
     end
@@ -51,6 +86,7 @@ class HuntServer
   end
 end
 
-server = HuntServer.new(31337)
+warn "The hunt server is still incomplete, sorry!"
+server = HuntServer.new(HUNTPORT)
 server.start
 
